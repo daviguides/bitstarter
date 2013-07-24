983d041ec80fe29da5a2dd7b749267d06542cdb8
Davi Guides <daviguides@kxserver.(none)>
2013-07-24 00:55:22 -0300

    changes on web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+	string = fs.readFileSync('index.html', 'utf8', function (err, data) {
+	  if (err) throw err;
+	  console.log(data);
+	});
+	response.send(string);
 });
 
 var port = process.env.PORT || 5000;
